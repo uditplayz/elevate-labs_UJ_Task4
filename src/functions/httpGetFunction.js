c9256b03adf6b2a2a6cad78f074a3ab31822d051
Udit Jain <uditplayz10@gmail.com>
2025-10-24 21:20:55 +0530

    Updated changes

--- src/functions/httpGetFunction.js
+++ src/functions/httpGetFunction.js
@@ -6,8 +6,8 @@ app.http('httpget', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        const name = request.query.get('name') || await request.text() || 'Udit';
 
-        return { body: `Hello, ${name}!` };
+        return { body: `Hello from ${name}! This is my first cloud function` };
     }
 });
